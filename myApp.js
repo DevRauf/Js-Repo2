bea93edcf3dba681367d6c3221f2fe3a326e038c
Rauf <tdm.tech23@gmail.com>
2023-03-01 18:09:09 +0500

    Java

--- myApp.js
+++ myApp.js
@@ -1,2 +1,18 @@
-console.log("Out of File")
-alert("This is alert")
+var btntranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+
+console.log(btntranslate);
+
+btntranslate.addEventListener("click", function clickEventHandler() {
+    console.log("Clicked")
+})
+
+// Exercise Lesson 4 Part 4
+
+var txtarea1 = document.querySelector("#txt-area1");
+
+
+var btnmybtn1 = document.querySelector(".btn-mybtn1");
+
+
+var inputbtn1 = document.querySelector("#input-btn1", [name = 'translator1']);
